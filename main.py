36185af34f08362e0ce36d167b1645e06d0e88cc
Swayam <Swayamsonawane1411@gmail.com>
2025-10-08 00:08:21 +0530

    final

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/hello")
 def read_hello():
     return {"message": "Hello from FastAPI on Railway!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
